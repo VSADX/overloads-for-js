7e9faf00fd0c52891b2a8d5a15f283032744a41b
VSADX <81491645+VSADX@users.noreply.github.com>
2021-08-05 00:24:35 -0500

    Create overloads.d.ts

---
+++ overloads.d.ts
@@ -0,0 +1,14 @@
+class Overload {
+    if<A,FN extends(a:A)=>unknown>(a:{new():A},func:FN):Overload
+    if<A,B,FN extends(a:A,b:B)=>unknown>(a:{new():A},b:{new():B},func:FN):Overload
+    if<A,B,C,FN extends(a:A,b:B,c:C)=>unknown>(a:{new():A},b:{new():B},c:{new():C},func:FN):Overload
+    if<A,B,C,D,FN extends(a:A,b:B,c:C,d:D)=>unknown>(a:{new():A},b:{new():B},c:{new():C},d:{new():D},func:FN):Overload
+    if<A,B,C,D,E,FN extends(a:A,b:B,c:C,d:D,e:E)=>unknown>(a:{new():A},b:{new():B},c:{new():C},d:{new():D},e:{new():E},func:FN):Overload
+    if<A,B,C,D,E,F,FN extends(a:A,b:B,c:C,d:D,e:E,f:F)=>unknown>(a:{new():A},b:{new():B},c:{new():C},d:{new():D},e:{new():E},f:{new():F},func:FN):Overload
+    if<A,B,C,D,E,F,G,FN extends(a:A,b:B,c:C,d:D,e:E,f:F,g:G)=>unknown>(a:{new():A},b:{new():B},c:{new():C},d:{new():D},e:{new():E},f:{new():F},g:{new():G},func:FN):Overload
+    
+    any(fn: Function):Overload
+
+    lock():Function
+}
+export const overload = () => new Overload
